327f89a39d72d3d8184e8fdef3feaa3bcb224ca8
Cam McKasson <cam@mckassons.com>
2017-05-23 20:08:26 -0700

    working on akai controller...

---
+++ Assets/_Scripts/Akai/AkaiController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+public class AkaiController : MonoBehaviour
+{
+    [SerializeField]
+    float m_RunCycleLegOffset = 0.2f; //specific to the character
+
+    private Animator m_animator;
+
+    private Rigidbody m_rigidBody;
+
+    private Vector2 m_move = Vector2.zero;
+
+    private float m_forward, m_turn;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_animator = GetComponent<Animator>();
+        if (m_animator == null)
+        {
+            Debug.Log("m_animator not found!");
+        }
+
+        m_rigidBody = GetComponent<Rigidbody>();
+        if (m_rigidBody == null)
+        {
+            Debug.Log("m_rigidBody not found!");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        UpdateAnimator();	
+	}
+
+    private void UpdateAnimator ()
+    {
+        //Debug.Log("m_forward == " + m_forward.ToString());
+        m_animator.SetFloat("Forward", m_forward);
+        m_animator.SetFloat("Turn", m_turn);
+
+        float runCycle = Mathf.Repeat(m_animator.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
+        float jumpLeg = (runCycle < 0.5f ? 1 : -1) * m_forward;
+
+        m_animator.SetFloat("JumpLeg", jumpLeg);
+        
+    }
+
+    private void OnAnimatorMove()
+    {
+        Vector3 v = m_animator.deltaPosition / Time.deltaTime;
+
+        // we preserve the existing y part of the current velocity.
+        v.y = m_rigidBody.velocity.y;
+        m_rigidBody.velocity = v;
+
+        transform.Rotate(m_animator.deltaRotation.eulerAngles);
+    }
+
+    public void Move (Vector2 move, bool fast)
+    {
+        m_move = Vector2.Lerp(m_move, move, 10.0f * Time.deltaTime);
+
+        m_forward = m_move.y;
+        m_turn = m_move.x;
+    }
+
+    public void Jump ()
+    {
+        Debug.Log("jump!");
+    }
+}
---
+++ Assets/_Scripts/Akai/AkaiControllerInput.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AkaiController))]
+public class AkaiControllerInput : MonoBehaviour
+{
+    [SerializeField]
+    private float m_jumpOrFastTime = 0.2f;
+
+    private AkaiController m_akaiController;
+
+    private Vector2 m_move = Vector2.zero;
+
+    private bool m_fast = false, m_fastCheck = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_akaiController = GetComponent<AkaiController>();
+        if (m_akaiController == null)
+        {
+            Debug.Log("m_akaiController not found!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        //Add control options here
+
+        GetXBOXcontrollerInput();
+        
+        m_akaiController.Move(m_move, m_fast);
+    }
+
+    private void GetXBOXcontrollerInput()
+    {
+        m_move.x = Input.GetAxis("Xaxis");
+        m_move.y = -Input.GetAxis("Yaxis");
+
+        Debug.Log("Input.GetAxis(Yaxis) == " + Input.GetAxis("Yaxis").ToString());
+
+        Debug.Log("m_move == " + m_move.ToString());
+
+        if (!m_fastCheck && Input.GetButton("button0"))
+        {
+            StartCoroutine(JumpOrFast());
+        }
+    }
+
+    private IEnumerator JumpOrFast()
+    {
+        m_fastCheck = true;
+
+        float endTime = Time.time + m_jumpOrFastTime;
+        while (Input.GetButton("button0") && endTime > Time.time)
+        {
+            yield return null;
+        }
+
+        if (Input.GetButton("button0"))
+        {
+            m_fast = true;
+
+            while (Input.GetButton("button0"))
+            {
+                yield return null;
+            }
+
+            m_fast = false;
+        }
+        else
+        {
+            m_akaiController.Jump();
+        }
+
+        m_fastCheck = false;
+        yield return null;
+    }
+}
